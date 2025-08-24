d5c29c0cd28d8e28dfc206d0bc9af6e6865263c5
MDRaquibulHasan <rh27.work@gmail.com>
2025-08-24 12:00:54 +0600

    commit

---
+++ test_code_display.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'config/db.php';
+require_once 'utils/code_helpers.php';
+
+echo "<h1>Code Display Test</h1>";
+echo "<p>This script tests the code helper functions to ensure they display the correct 6-digit codes for different entity IDs.</p>";
+
+// Test with some sample data
+echo "<h2>Testing Code Helper Functions</h2>";
